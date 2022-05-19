3250610f77a4b008e0243e623c06e6fef4dd510a
SpecificKarma <specifickarma@gmail.com>
2022-05-19 13:38:11 -0700

    get add firebase

--- sandbox.js
+++ sandbox.js
@@ -1,10 +1,37 @@
-
 const ul = document.getElementById("tasks");
-const textField = document.getElementById("formField");
+const form = document.getElementById("form");
+const timeEdit = document.getElementById("formTime");
+const activityEdit = document.getElementById("formActivity");
+const submitBtn = document.getElementById("submit");
 
-getData();
+const getData = () => {
+    db.collection('tasks').get()
+        .then(
+            (snapshot) => {
+                console.log("getData", true);
+                showTasks(snapshot.docs);
+            })
+        .catch(
+            err => {
+                console.log(err);
+            });
+};
+
+const addData = (obj) => {
+    db.collection('tasks').add(obj)
+        .then(() => {
+            getData();
+        })
+        .catch(
+            err => {
+                console.log(err);
+            });
+};
 
 const showTasks = (tasks) => {
+    if (ul.childNodes.length > 0) {
+        ul.innerHTML = '';
+    }
     tasks.forEach(task => {
         let li = document.createElement('li');
         li.textContent = `${task.data().action} ${task.data().time}`;
@@ -12,43 +39,53 @@ const showTasks = (tasks) => {
     });
 };
 
-textField.addEventListener('keypress', (e) => {
-    e.preventDefault();
+let eventList = ["keypress", "click"];
 
-    if (e.key === 'Enter') {
-        // code for enter
-        const data = {
-            time: "18:00",
-            action: textField.value
-        };
+for (e of eventList) {
+    form.addEventListener(e, listener);
+}
 
-        console.log(this.value);
+function listener(e) {
 
+    function isNotEmpty(...args) {
+        let conditions = [];
+        for (let i = 0; i < args.length; i++) {
+            if (args[i].value.length > 0) {
+                args[i].value = "";
+                conditions[i] = true;
+            }
+        }
+        return conditions.length === args.length;
+    }
+
+    let data = null;
+    switch (e.target.id) {
+        case timeEdit.id:
+        case activityEdit.id:
+            if (e.type === "keypress" && e.key === 'Enter') {
+                if (isNotEmpty(timeEdit, activityEdit)) {
+                    data = {
+                        time: timeEdit.value,
+                        action: activityEdit.value
+                    };
+                }
+            }
+            break;
+        case submitBtn.id:
+            if (e.type === "click") {
+                e.preventDefault();
+                if (isNotEmpty(timeEdit, activityEdit)) {
+                    data = {
+                        time: timeEdit.value,
+                        action: activityEdit.value
+                    };
+                }
+            }
+            break;
+    }
+    if (data != null) {
         addData(data);
     }
-});
+}
 
-getData = ()=> {
-    db.collection('tasks').get()
-    .then(
-        (snapshot) => {
-            showTasks(snapshot.docs);
-        })
-    .catch(
-        err => {
-            console.log(err);
-        });
-};
-
-addData = (obj) => {
-    db.collection('tasks').add(obj)
-    .then(
-        (snapshot) => {
-            showTasks(snapshot.docs);
-        })
-    .catch(
-        err => {
-            console.log(err);
-        });
-};
-
+getData();
